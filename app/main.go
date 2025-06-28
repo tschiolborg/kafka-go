4ee41a33836d8ead579c977bce110f11aa986028
tschiolborg <66739466+tschiolborg@users.noreply.github.com>
2025-06-29 00:55:10 +0200

    tcp

--- app/main.go
+++ app/main.go
@@ -1,8 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
 func main() {
-	// This is a placeholder main function.
-	// You can add your application logic here.
-	println("Hello, Kafka!")
+	l, err := net.Listen("tcp", "0.0.0.0:9092")
+	if err != nil {
+		fmt.Println("Failed to bind to port 9092")
+		os.Exit(1)
+	}
+	c, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Successfully bound to port 9092 and accepted a connection")
+
+	// Make sure to use buf[:n] slice to avoid reading stale data
+	buf := make([]byte, 1024)
+	for {
+		n, err := c.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			break
+		}
+		if n > 0 {
+			fmt.Printf("Received data: %s\n", string(buf[:n]))
+		}
+	}
 
 }
