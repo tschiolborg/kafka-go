cd6ce493e826f7c6831bd76a3452caf859849827
tschiolborg <66739466+tschiolborg@users.noreply.github.com>
2025-06-29 00:38:00 +0200

    main

---
+++ README.md
@@ -0,0 +1,8 @@
+# Kafka in Go
+
+```bash
+go run ./app
+go build -o ./bin/app ./app
+
+golangci-lint run
+```
---
+++ app/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// This is a placeholder main function.
+	// You can add your application logic here.
+	println("Hello, Kafka!")
+
+}
